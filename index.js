c77d4cea6f5f575d40358536b359196e4c830050
Bianca Gonçalves <biancagoncalves@MacBook-Pro.local>
2021-03-24 22:58:51 -0700

    finished, deploy next

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const { hash } = window.location
+
+const message = (atob(hash.replace('#', ''))) //replaces hash with '' then decodes message back
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide')
+    document.querySelector('#message-show').classList.remove('hide')
+
+    document.querySelector('h1').innerHTML = message
+}
+
 document.querySelector('form').addEventListener('submit', e =>{
     e.preventDefault() //prevents the default of browser submitting form and refreshing
 
@@ -10,4 +21,5 @@ document.querySelector('form').addEventListener('submit', e =>{
     const linkInput = document.querySelector('#link-input') //selecting 2nd input
     linkInput.value = `${window.location}#${encrypted}` //displays the encoded message LINK in input
     linkInput.select() //selects the url link input
+
 })
